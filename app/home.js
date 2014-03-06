29a9038d481c7c084f59e324f579fd70c845a89f
Boyko Ant <boyko.ant@gmail.com>
2014-03-06 20:49:02 +0200

    custom api

--- app/home.js
+++ app/home.js
@@ -1,6 +1,28 @@
 define(function (require) {
-    var model = {
+    var ko = require('knockout');
+    require('wams');
 
+    var appUrl = 'https://webinar-onlineshop.azure-mobile.net/';
+    var appKey = 'ZHNxhVbjQhZTzUcMuXVQpugLVCIEEv25';
+
+    var client = new WindowsAzure.MobileServiceClient(appUrl, appKey);
+
+    var model = {
+        message: ko.observable(''),
+
+        activate: function() {
+            client.invokeApi('ping', {
+                body: null,
+                method: "get"
+            }).done(
+                function(data) {
+                    model.message(data.result.message);
+                },
+                function(error){
+                    alert(error);
+                }
+            );
+        }
     };
 
     return model;
